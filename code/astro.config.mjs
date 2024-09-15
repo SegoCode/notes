ab271988bc6c1bd519d7d4d26669bcdea4aeee90
SegoCode <35817798+SegoCode@users.noreply.github.com>
2024-09-15 17:57:55 +0200

    Update astro.config.mjs

--- code/astro.config.mjs
+++ code/astro.config.mjs
@@ -8,21 +8,46 @@ export default defineConfig({
   	base: 'notes',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Mis notas',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Git',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ slug: 'git/instalar' },
+						{ slug: 'git/firmar' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'SVN',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Compilaciones',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Despliegues',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Certificados',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Snippeds',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Marcadores',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Guias',
+					autogenerate: { directory: 'guides' },
 				},
 			],
 		}),
