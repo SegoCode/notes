2755b9f5a53b144f3abfc0818ddce0c7ae71f54a
SegoCode <35817798+SegoCode@users.noreply.github.com>
2024-09-15 13:49:01 +0200

    Update astro.config.mjs

--- code/astro.config.mjs
+++ code/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://segocode.github.io',
+  	base: 'notes',
 	integrations: [
 		starlight({
 			title: 'My Docs',
